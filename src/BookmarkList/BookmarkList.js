4dc325320d8daf924e0ba34885862a72d8dbbc5d
Alex Putnam <a.kk.putnam@gmail.com>
2020-12-19 17:52:22 -0800

    prop-types added

--- src/BookmarkList/BookmarkList.js
+++ src/BookmarkList/BookmarkList.js
@@ -2,35 +2,33 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import BookmarksContext from '../BookmarksContext';
 import BookmarkItem from '../BookmarkItem/BookmarkItem';
-import './BookmarkList.css'
+import './BookmarkList.css';
 
 class BookmarkList extends Component {
-  static proptTypes = {
+  static propTypes = {
     bookmarks: PropTypes.arrayOf(
       PropTypes.shape({
-        id: PropTypes.string,
+        id: PropTypes.oneOfType([PropTypes.number, PropTypes.string])
+          .isRequired,
       })
-    )
+    ),
   };
 
   static defaultProps = {
-    bookmarks: []
+    bookmarks: [],
   };
 
   static contextType = BookmarksContext;
 
   render() {
-    const { bookmarks } = this.context
+    const { bookmarks } = this.context;
     return (
       <section className='BookmarkList'>
         <h2>Your bookmarks</h2>
         <ul className='BookmarkList__list' aria-live='polite'>
-          {bookmarks.map(bookmark =>
-            <BookmarkItem
-              key={bookmark.id}
-              {...bookmark}
-            />
-          )}
+          {bookmarks.map((bookmark) => (
+            <BookmarkItem key={bookmark.id} {...bookmark} />
+          ))}
         </ul>
       </section>
     );
